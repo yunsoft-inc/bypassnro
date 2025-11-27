fc1a81fafbafc672c92320d5964e7562871cbdf8
JaeWoong Yun <61818894+yunsoft-inc@users.noreply.github.com>
2025-11-27 17:08:46 +0900

    Update URL and filename in bypass.cmd

--- bypass.cmd
+++ bypass.cmd
@@ -1,2 +1,2 @@
-curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/ChrisTitusTech/bypassnro/refs/heads/main/unattend.xml
+curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/yunsoft-inc/bypassnro/refs/heads/main/autounattend.xml
 %WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\unattend.xml /reboot
